0cfa9684c100bb428cd530ee9779e4746891d4d3
tjanuki <t.januki@gmail.com>
2025-05-30 17:11:55 -0400

    Improve menu bar clock layout and positioning

--- JSTClockMenuBar/ClockView.swift
+++ JSTClockMenuBar/ClockView.swift
@@ -4,15 +4,16 @@ struct ClockView: View {
     @ObservedObject var clockManager: ClockManager
     
     var body: some View {
-        VStack(spacing: -2) {
+        VStack(spacing: -3) {
             Text(clockManager.dateString)
-                .font(.system(size: 9, weight: .regular))
+                .font(.system(size: 10, weight: .regular))
                 .foregroundColor(.primary)
             
             Text(clockManager.timeString)
-                .font(.system(size: 11, weight: .medium))
+                .font(.system(size: 10, weight: .regular))
                 .foregroundColor(.primary)
         }
         .frame(width: 50, height: 30)
+        .offset(y: -4)
     }
 }
